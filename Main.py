b76fa8803ae41098c3b16d8f464c77d4a7186455
jarvis <jarvis_smith@charter.net>
2017-07-27 14:17:55 -0700

    Newest commit.

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def main(msg):
 <<<<<<< HEAD
+    // Another comment
     // Adding a different comment here
     // Print a message
 >>>>>>> refs/remotes/origin/master
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # python-pi-example
 Stupid Example python
 This is just a stupid example for a neub on the use of git on ubuntu.
+
+
+Make a chang here.
